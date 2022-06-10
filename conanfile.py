092c4cceae84455ec8e41b112778deb68c8c4ed7
Ulrich Eck <ulrich.eck@tum.de>
2022-06-10 08:43:16 +0200

    upgraded to qt5, changed include paths, build with cmake

--- conanfile.py
+++ conanfile.py
@@ -1,59 +1,142 @@
-from conans import ConanFile, tools
+from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
 import os
-import shutil
+import textwrap
+
 
 class QtPropertyBrowserConan(ConanFile):
     name = "qt-propertybrowser"
-    version = "1.0"
-    generators = "qmake"
+    version = "2.0"
     settings = "os", "arch", "compiler", "build_type"
 
-    options = {"shared": [True, False]}
-    default_options = "shared=True"
 
-    license = "Copyright ??"
-    description = "collection of qt widgets"
+    generators = ["cmake", "cmake_find_package"]
+    exports_sources = ["patches/**","CMakeLists.txt"]
+    _cmake = None
 
-    requires = (
-            "qt/5.15.4",
-    )
+    options = {
+         "shared": [True, False],
+         "fPIC": [True, False]
+    }
+    default_options = {
+        "shared":      False,
+        "fPIC": True
+    }
+
+    license = "Copyright by qt-solutions git repo"
+    description = "qt property browser"
 
     scm = {
         "type": "git",
-        "subfolder": "source",
-        "url": "https://github.com/ulricheck/qt-solutions.git",
-        "revision": "master",
+        "subfolder": "source_subfolder",
+        "url": "https://github.com/TUM-CAMP-NARVIS/qtpropertybrowser.git",
+        "revision": "main",
         "submodule": "recursive",
     }
 
 
+    @staticmethod
+    def _create_cmake_module_alias_targets(module_file, alias, aliased):
+        content = ""
+        content += textwrap.dedent("""\
+            if(TARGET {aliased} AND NOT TARGET {alias})
+                add_library({alias} INTERFACE IMPORTED)
+                set_property(TARGET {alias} PROPERTY INTERFACE_LINK_LIBRARIES {aliased})
+            endif()
+        """.format(alias=alias, aliased=aliased))
+        tools.save(module_file, content)
+
+    @property
+    def _module_subfolder(self):
+        return os.path.join(
+            "lib",
+            "cmake"
+        )
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_folder(self):
+        return "build"
+
+    @property
+    def _pkg_share(self):
+        return os.path.join(
+            self.package_folder,
+            "share"
+        )
+
+    @property
+    def _pkg_etc(self):
+        return os.path.join(
+            self.package_folder,
+            "etc"
+        )
+    
+    @property
+    def _pkg_res(self):
+        return os.path.join(
+            self.package_folder,
+            "res"
+        )
+
+    @property
+    def _pkg_cmake(self):
+        return os.path.join(
+            self.package_folder,
+            "lib/cmake"
+        )
+
+    @property
+    def _target_aliases(self):
+        aliases = {
+        }
+        return aliases
+
+    def _patch_sources(self):
+        for patch in []:  #[{"base_path": self._source_subfolder, "patch_file":"patches/patch_gcc11_compatibility.patch",  "strip":0},]:
+            tools.patch(**patch)
+
+    def configure(self):
+        if self.settings.compiler == 'Visual Studio':
+            del self.options.fPIC
+
+    def config_options(self):
+        pass
+    
+    def requirements(self):
+        self.requires("qt/5.15.4")
+
+    def validate(self):
+        os = self.settings.os
+        compiler = self.settings.compiler
+        version = tools.Version(self.settings.compiler.version)
+        if compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, 11)
+        if compiler == "Visual Studio" and version < "16":
+            raise ConanInvalidConfiguration(
+                "QTpropertybrowser is just supported for Visual Studio compiler 16 and higher.")
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        if self.settings.compiler != 'Visual Studio':
+            self._cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
+        self._cmake.configure()
+        return self._cmake
 
     def build(self):
-        folder_name = os.path.join("source", "qtpropertybrowser")
-        open(os.path.join(folder_name, "config.pri"), "w").write("SOLUTIONS_LIBRARY = yes")
-        tools.replace_in_file(os.path.join(folder_name, "qtpropertybrowser.pro"), 
-            """CONFIG += ordered""",
-            """CONFIG += ordered
-CONFIG += conan_basic_setup
-include(../../conanbuildinfo.pri)"""
-            )
-        self.run( "cd %s && qmake CONFIG+=debug_and_release" % folder_name, run_environment=True)
-        if self.settings.compiler == "Visual Studio":
-            if self.settings.build_type == "Debug":
-                self.run( "cd %s && nmake debug" % folder_name, run_environment=True )
-            else:
-                self.run( "cd %s && nmake release" % folder_name, run_environment=True )
-        else:
-            if self.settings.build_type == "Debug":
-                self.run( "cd %s && make debug" % folder_name, run_environment=True )
-            else:
-                self.run( "cd %s && make release" % folder_name, run_environment=True )
+        self._patch_sources()
+        cmake = self._configure_cmake()
+        cmake.build()
 
     def package(self):
-        self.copy(pattern="*.h", dst="include", src=os.path.join("source", "qtpropertybrowser", "src"))
-        self.copy(pattern="*.so*", dst="lib", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
+        cmake = self._configure_cmake()
+        cmake.install()
+        self.copy("LICENSE", src=self._source_subfolder, dst="licenses")
         
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
